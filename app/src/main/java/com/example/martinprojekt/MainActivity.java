3e03c8377fa8fbb78a65cf68d285961acda404e5
martin_rydahl@hotmail.com <9notsAjtjLfFxdR>
2019-05-21 18:48:22 +0200

    ListAdapter Buttons

--- app/src/main/java/com/example/martinprojekt/MainActivity.java
+++ app/src/main/java/com/example/martinprojekt/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.martinprojekt;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+
 
 
 
@@ -18,26 +22,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView mListView = (ListView) findViewById(R.id.listView);
 
         mViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
         mViewModel.getUsers().observe(this, users -> {
 
             Log.d("her er dataen", "onActivityCreated: " + users.toString());
 
-            ListView listView = findViewById(R.id.listView);
-                String [] userData = new String[users.size()];
-                for (int b = 0; b<users.size(); b++){
-                    userData[b] = users.get(b).getName();
-
-                }
-                listView.setAdapter(
-                        new ArrayAdapter<String>(
-                                getApplicationContext(),
-                                android.R.layout.simple_list_item_1,
-                                userData)
-
-                );
-
+            UserListAdapter adapter = new UserListAdapter(this, R.layout.adapter_view_layout, users);
+            mListView.setAdapter(adapter);
+        });
+        Button mButton = findViewById(R.id.goBack);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Welcome.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
